5e2efb4d06b32e92d3dc4ba6adf6af36dc714922
Mystasly <mystasly48@gmail.com>
2016-04-24 15:59:32 +0900

    changed file name, study to program.

--- Program.java
+++ Program.java
@@ -1,8 +1,8 @@
-class Study {
+class Program {
   public static void main (String[] args) {
     System.out.println("3 Billion Devices Run Java");
   }
-  
+
   public static void tajigen1() {
     int[][] a = new int[2][3];
     int[][] b = {
@@ -14,8 +14,8 @@ class Study {
     System.out.println(a[1][1]);
     System.out.println(b[1][1]);
   }
-  
-  public static void tajigen2() {    
+
+  public static void tajigen2() {
     int[][] tajigenArray = {
       { 0, 1, 2},
       { 3, 4, 5}
@@ -24,7 +24,7 @@ class Study {
     System.out.println(tajigenArray[0][2]);
     System.out.println(tajigenArray[1][0]);
     System.out.println(tajigenArray[1][2]);
-    
+
     /*
     int[][] tajigenArray = new int[2][3];
     tajigenArray[0][0] = 0;
@@ -33,7 +33,7 @@ class Study {
     tajigenArray[1][0] = 3;
     tajigenArray[1][1] = 4;
     tajigenArray[1][2] = 5;
-    
+
     System.out.println(tajigenArray[0][0]);
     System.out.println(tajigenArray[0][2]);
     System.out.println(tajigenArray[1][0]);
