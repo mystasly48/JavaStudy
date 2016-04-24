5de681f7039a7d5beec623b6e4c0d82c8e12abcb
Mystasly <mystasly48@gmail.com>
2016-04-24 14:29:15 +0900

    added a source files

---
+++ Episode4.java
@@ -0,0 +1,9 @@
+class Episode4 {
+  public static void main (String[] args) {
+    System.out.println("3 Billion Devices Run Java");
+  }
+  
+  public static void question1() {
+    
+  }
+}
---
+++ Study.java
@@ -0,0 +1,43 @@
+class Study {
+  public static void main (String[] args) {
+    System.out.println("3 Billion Devices Run Java");
+  }
+  
+  public static void tajigen1() {
+    int[][] a = new int[2][3];
+    int[][] b = {
+      { 1, 2 },
+      { 3, 4 },
+      { 5, 6 }
+    };
+    a[1] [1] = 10;
+    System.out.println(a[1][1]);
+    System.out.println(b[1][1]);
+  }
+  
+  public static void tajigen2() {    
+    int[][] tajigenArray = {
+      { 0, 1, 2},
+      { 3, 4, 5}
+    };
+    System.out.println(tajigenArray[0][0]);
+    System.out.println(tajigenArray[0][2]);
+    System.out.println(tajigenArray[1][0]);
+    System.out.println(tajigenArray[1][2]);
+    
+    /*
+    int[][] tajigenArray = new int[2][3];
+    tajigenArray[0][0] = 0;
+    tajigenArray[0][1] = 1;
+    tajigenArray[0][2] = 2;
+    tajigenArray[1][0] = 3;
+    tajigenArray[1][1] = 4;
+    tajigenArray[1][2] = 5;
+    
+    System.out.println(tajigenArray[0][0]);
+    System.out.println(tajigenArray[0][2]);
+    System.out.println(tajigenArray[1][0]);
+    System.out.println(tajigenArray[1][2]);
+    */
+  }
+}
