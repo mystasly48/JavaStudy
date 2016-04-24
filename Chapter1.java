403f62f4cbb47d57fc274cabb4e1d2b89f1225f2
Mystasly <mystasly48@gmail.com>
2016-04-24 16:19:09 +0900

    completed chapter one questions.

--- Chapter1.java
+++ Chapter1.java
@@ -1,9 +1,18 @@
 class Chapter1 {
   public static void main (String[] args) {
-    System.out.println("3 Billion Devices Run Java");
+    question3();
   }
 
   public static void question1() {
+    System.out.println("Hello World!");
+  }
 
+  public static void question2() {
+    // 出力する
+    System.out.println("こんにちは日本");
+  }
+
+  public static void question3() {
+    System.out.println("Javaの学習を始めます");
   }
 }
