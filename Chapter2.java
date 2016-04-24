ef264c445af36518c3b2c0e63ae9d5efe31d90b6
Mystasly <mystasly48@gmail.com>
2016-04-24 16:50:12 +0900

    Revert "completed chapter two questions."
    This reverts commit 7e118e79ef7820fb55120b6c6bee19fec22bb6c6.

--- Chapter2.java
+++ Chapter2.java
@@ -1,6 +1,6 @@
 class Chapter2 {
   public static void main (String[] args) {
-    question7();
+    System.out.println("3 Billion Devices Run Java");
   }
 
   public static void question1() {
@@ -29,37 +29,6 @@ class Chapter2 {
   }
 
   public static void question4() {
-    int [][] a = new int[2][3];
-    byte [][] b = {
-      { 1, 2 },
-      { 3, 4 },
-      { 5, 6 }
-    };
-    a[1][1] = 10;
-    System.out.println(a[0][0]);
-    System.out.println(b[1][1]);
 
-    // 出力結果
-    // 0
-    // 4
-    // 理由
-    // 配列 a[0][0] には初期化のみで代入はされていなかった。
-    // 配列 b[1][0] には 3 が、配列 b[1][1] には 4 が代入されていた。
-  }
-
-  public static void question5() {
-    int a = 522 + (105 * 2) + 155;
-    System.out.println(a);
-  }
-
-  public static void question6() {
-    double a = (498 + (98 * 3) + 298) * 1.05;
-    System.out.println(a);
-  }
-
-  public static void question7() {
-    // (1 + 2 + 3 + 4 + 5) / 5 = 3
-    int a = (82 + 78 + 65 + 59 + 77) / 5;
-    System.out.println(a);
   }
 }
