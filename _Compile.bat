af05587f845c5d8c313f78563739ca9b07c0f59d
Mystasly <mystasly48@gmail.com>
2016-04-24 14:28:51 +0900

    added a batch files

---
+++ _Compile.bat
@@ -0,0 +1,7 @@
+javac Episode1.java
+javac Episode2.java
+javac Episode3.java
+javac Episode4.java
+javac Episode5.java
+javac Study.java
+pause
---
+++ _RunAll.bat
@@ -0,0 +1,7 @@
+java Episode1
+java Episode2
+java Episode3
+java Episode4
+java Episode5
+java Study
+pause
