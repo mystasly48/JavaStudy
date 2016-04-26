db259c3bdf4e38b5b36df96a05403fdf746ef8b2
Mystasly <mystasly48@gmail.com>
2016-04-26 22:05:10 +0900

    ソースファイル名変えたからコッチも変えたよん

--- _Compile.bat
+++ _Compile.bat
@@ -1,7 +1,10 @@
-javac Episode1.java
-javac Episode2.java
-javac Episode3.java
-javac Episode4.java
-javac Episode5.java
-javac Study.java
+javac Chapter1.java
+javac Chapter2.java
+javac Chapter3.java
+javac Chapter4.java
+javac Chapter5.java
+javac Chapter6.java
+javac Chapter7.java
+javac FinalPractice.java
+javac Program.java
 pause
--- _RunAll.bat
+++ _RunAll.bat
@@ -1,7 +1,10 @@
-java Episode1
-java Episode2
-java Episode3
-java Episode4
-java Episode5
-java Study
+java Chapter1
+java Chapter2
+java Chapter3
+java Chapter4
+java Chapter5
+java Chapter6
+java Chapter7
+java FinalPractice
+java Program
 pause
